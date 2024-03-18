0d8bffc879c83a2f664c01b2d4d57ebbb34b2055
nima asoud <nima.asoudeh@gmail.com>
2024-03-18 12:40:21 +0330

    webapp/urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('matches/', include('matches.urls')),
+    path('news/', include('news.urls')),
+    path('', include('tables.urls')),
+    path('', include('fixtures.urls'))
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
