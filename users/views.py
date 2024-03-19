9fa791953ba78a31f97ebbd1726535e8bd5c0e8f
nima asoud <nima.asoudeh@gmail.com>
2024-03-19 20:18:42 +0330

    update requirements.txt

--- users/views.py
+++ users/views.py
@@ -1,33 +1,35 @@
-from rest_framework import generics, status
+from rest_framework import generics,status,views,permissions
 from rest_framework.response import Response
-from .serializers import RegisterSerializer, LoginSerializer, LogoutSerializer
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from django.views.decorators.csrf import csrf_exempt
+from .serializers import RegisterSerializer,LoginSerializer,LogoutSerializer
+from rest_framework.decorators import api_view, permission_classes
+from django.views.decorators.csrf import csrf_protect
+# Create your views here.
 
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
-
-    def post(self, request):
-        user = request.data
+    def post(self,request):
+        user=request.data
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         user_data = serializer.data
         return Response(user_data, status=status.HTTP_201_CREATED)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@csrf_protect
 class LoginAPIView(generics.GenericAPIView):
     serializer_class = LoginSerializer
-
-    @csrf_exempt
-    def post(self, request):
+    def post(self,request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
 class LogoutAPIView(generics.GenericAPIView):
     serializer_class = LogoutSerializer
-    permission_classes = (IsAuthenticated,)
-
+    permission_classes = (permissions.IsAuthenticated,)
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
