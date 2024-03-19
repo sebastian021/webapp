d50feec7a4f134946ea5bdabe760afcb4b469726
nima asoud <nima.asoudeh@gmail.com>
2024-03-19 11:56:45 +0330

    change users views.py and urls.py

--- users/views.py
+++ users/views.py
@@ -1,35 +1,32 @@
-from rest_framework import generics,status,views,permissions
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from .serializers import RegisterSerializer, LoginSerializer, LogoutSerializer
 from rest_framework.permissions import IsAuthenticated
-from .serializers import RegisterSerializer,LoginSerializer,LogoutSerializer
-from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_protect
-# Create your views here.
 
 class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
-    def post(self,request):
-        user=request.data
+
+    def post(self, request):
+        user = request.data
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         user_data = serializer.data
         return Response(user_data, status=status.HTTP_201_CREATED)
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-@csrf_protect
 class LoginAPIView(generics.GenericAPIView):
     serializer_class = LoginSerializer
-    def post(self,request):
+
+    def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class LogoutAPIView(generics.GenericAPIView):
     serializer_class = LogoutSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
+
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
